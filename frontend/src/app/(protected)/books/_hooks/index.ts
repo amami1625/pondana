abfa4c5e1670205463b9864a83e53055c57ccd86
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-26 22:16:17 +0900

    fix: カスタムフックを対応するドメインのディレクトリに移動

--- frontend/src/app/(protected)/books/_hooks/index.ts
+++ frontend/src/app/(protected)/books/_hooks/index.ts
@@ -1,4 +1,2 @@
 export { useDeleteBook } from './useDeleteBook';
-export { useCategoryModal } from './useCategoryModal';
-export { useAuthorModal } from './useAuthorModal';
 export { useBookFormState } from './useBookFormState';
