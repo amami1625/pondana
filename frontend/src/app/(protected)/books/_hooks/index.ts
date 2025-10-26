ce169a706f54865ed7ece63b555917af6467fc66
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-26 21:46:01 +0900

    refactor: モーダルの状態管理を行う汎用のカスタムフックを作成

--- frontend/src/app/(protected)/books/_hooks/index.ts
+++ frontend/src/app/(protected)/books/_hooks/index.ts
@@ -1,6 +1,4 @@
-export { useUpdateForm } from './useUpdateForm';
 export { useDeleteBook } from './useDeleteBook';
-export { useCreateBook } from './useCreateBook';
 export { useCategoryModal } from './useCategoryModal';
 export { useAuthorModal } from './useAuthorModal';
 export { useBookFormState } from './useBookFormState';
