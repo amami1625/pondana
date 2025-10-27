e018a2f799e6577d16f85fb030d3182e7a57e533
amami1625 <amami_gt0403@yahoo.co.jp>
2025-10-27 20:31:19 +0900

    refactor: ユーザーデータの型を追加

--- frontend/src/schemas/profile.ts
+++ frontend/src/schemas/profile.ts
@@ -5,6 +5,16 @@ export const profileSchema = z.object({
   supabase_uid: z.string(),
   name: z.string(),
   avatar_url: z.string().nullable(),
-  created_at: z.string(),
-  updated_at: z.string(),
+  created_at: z.string().transform((str) => {
+    return new Date(str).toLocaleString('ja-JP', {
+      timeZone: 'Asia/Tokyo',
+    });
+  }),
+  updated_at: z.string().transform((str) => {
+    return new Date(str).toLocaleString('ja-JP', {
+      timeZone: 'Asia/Tokyo',
+    });
+  }),
 });
+
+export type User = z.infer<typeof profileSchema>;
