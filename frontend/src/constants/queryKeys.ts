229262d6cabf45c7fdc18ae554f5e5f7d773e352
amami1625 <amami_gt0403@yahoo.co.jp>
2025-11-03 22:46:29 +0900

    feat: 本のクエリキーを設定

--- frontend/src/constants/queryKeys.ts
+++ frontend/src/constants/queryKeys.ts
@@ -9,4 +9,7 @@ export const queryKeys = {
     all: ['lists'] as const, // 一覧取得用
     detail: (id: number) => ['lists', 'detail', id] as const, // 詳細取得用
   },
+  books: {
+    all: ['books'] as const, // 一覧取得用
+  },
 } as const;
