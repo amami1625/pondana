710eee0048555945a74799ef14b8cdd339ab48e1
amami1625 <amami_gt0403@yahoo.co.jp>
2025-11-14 02:00:33 +0900

    refactor: ユーザーデータを削除

--- frontend/src/schemas/top.ts
+++ frontend/src/schemas/top.ts
@@ -1,12 +1,10 @@
 import { z } from 'zod';
-import { userSchema } from './user';
 import { bookSchema } from './book';
 import { listSchema } from './list';
 import { cardSchema } from './card';
 
 // トップページデータのバリデーションスキーマ
 export const topPageSchema = z.object({
-  profile: userSchema,
   recent_books: z.array(bookSchema).max(5),
   recent_lists: z.array(listSchema).max(5),
   recent_cards: z
