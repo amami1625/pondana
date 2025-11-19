bd9b78c7bf7cfacecdb6cf16d3380465f393f918
amami1625 <amami_gt0403@yahoo.co.jp>
2025-11-19 21:26:39 +0900

    test: ユーザーのファクトリーを作成

--- frontend/src/test/factories/index.ts
+++ frontend/src/test/factories/index.ts
@@ -3,3 +3,4 @@ export { createMockAuthor } from './author';
 export { createMockCategory } from './category';
 export { createMockList } from './list';
 export { createMockCard } from './card';
+export { createMockUser } from './user';
