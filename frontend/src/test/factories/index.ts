2e395cad382d176f8cc8404ebaf7a44d94269744
amami1625 <amami_gt0403@yahoo.co.jp>
2025-11-20 18:45:40 +0900

    test: ListBook のファクトリーを作成

--- frontend/src/test/factories/index.ts
+++ frontend/src/test/factories/index.ts
@@ -2,6 +2,7 @@ export { createMockBook } from './book';
 export { createMockAuthor } from './author';
 export { createMockCategory } from './category';
 export { createMockList } from './list';
+export { createMockListBook } from './listBook';
 export { createMockCard } from './card';
 export { createMockUser } from './user';
 export { createMockTopPageData } from './topPage';
