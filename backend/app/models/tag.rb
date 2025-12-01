ce9c54bc16aae667aea01e9e6d28fbaaa3039638
amami1625 <amami_gt0403@yahoo.co.jp>
2025-12-01 21:04:49 +0900

    feat: tags のコントローラーとモデルを作成

---
+++ backend/app/models/tag.rb
@@ -0,0 +1,7 @@
+class Tag < ApplicationRecord
+  belongs_to :user
+  has_many :book_tags, dependent: :destroy
+  has_many :books, through: :book_tags
+
+  validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 100 }
+end
