e8eb30e96d04154ac53c5e2640d4c916a4530b74
amami1625 <amami_gt0403@yahoo.co.jp>
2025-12-06 14:37:48 +0900

    refactor: クエリキーで使用する id を uuid へ変更

--- frontend/src/constants/queryKeys.ts
+++ frontend/src/constants/queryKeys.ts
@@ -10,15 +10,15 @@ export const queryKeys = {
   },
   lists: {
     all: ['lists'] as const, // 一覧取得用
-    detail: (id: number) => ['lists', 'detail', id] as const, // 詳細取得用
+    detail: (id: string) => ['lists', 'detail', id] as const, // 詳細取得用
   },
   books: {
     all: ['books'] as const, // 一覧取得用
-    detail: (id: number) => ['books', 'detail', id] as const, // 詳細取得用
+    detail: (id: string) => ['books', 'detail', id] as const, // 詳細取得用
   },
   cards: {
     all: ['cards'] as const, // 一覧取得用
-    detail: (id: number) => ['cards', 'detail', id] as const, // 詳細取得用
+    detail: (id: string) => ['cards', 'detail', id] as const, // 詳細取得用
   },
   profile: {
     all: ['profile'] as const, // 自分のプロフィール取得用
