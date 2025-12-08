b92d3333f8ec54cf049f4de35eb8032ed2d49d5b
amami1625 <amami_gt0403@yahoo.co.jp>
2025-12-08 13:07:07 +0900

    remove: authors テーブルと book_authors テーブルを削除

--- backend/app/models/author.rb
+++
@@ -1,7 +0,0 @@
-class Author < ApplicationRecord
-  belongs_to :user
-  has_many :book_authors, dependent: :destroy
-  has_many :books, through: :book_authors
-
-  validates :name, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 100 }
-end
